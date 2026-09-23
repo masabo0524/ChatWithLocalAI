14e6d7b6aab1d517b922c7b971e85dc9e9722f82
masabo0524 <masabo0524@gmail.com>
2026-09-23 18:16:46 +0900

    add ask AI function

--- DjangoChannel/channel/consumers.py
+++ DjangoChannel/channel/consumers.py
@@ -34,30 +34,36 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        isAskAI = text_data_json['isAskAI']
         received_time = datetime.datetime.now()
         received_time_format = received_time.strftime("%Y-%m-%d %H:%M")
         self.send(text_data=json.dumps({"isMessage": True, "message":
                                         {"context": message, "sender": self.user.username, "time": received_time_format, "fromMe": True }}))
         received_message = Message(room=self.room_instance, at_received=received_time, sender=self.user, context=message)
         received_message.save()
-        self.send(text_data=json.dumps({"isMessage": False, "status": "thinking"}))
         
-        response = chat(
-            model='gemma4:e4b',
-            messages=[
-                {
-                    "role": 'user',
-                    'content': message,
-                }
-            ],
-        )
-        sending_time = datetime.datetime.now()
-        sending_time_format = sending_time.strftime("%Y-%m-%d %H:%M")
-        self.send(text_data=json.dumps({"isMessage": True, "message":
-                                        {"context": response.message.content, "sender": "AI", "time": sending_time_format, "fromMe": False }}))
-        sending_message = Message(room=self.room_instance, at_received=received_time, sender=self.user, context=response.message.content, reply_byAI=True)
-        sending_message.save()
-        self.send(text_data=json.dumps({"isMessage": False, "status": "wait4you"}))
+        if(isAskAI):
+
+            self.send(text_data=json.dumps({"isMessage": False, "status": "thinking"}))
+        
+            response = chat(
+                model='gemma4:e4b',
+                messages=[
+                    {
+                        "role": 'user',
+                        'content': message,
+                    }
+                ],
+            )
+            sending_time = datetime.datetime.now()
+            sending_time_format = sending_time.strftime("%Y-%m-%d %H:%M")
+            self.send(text_data=json.dumps({"isMessage": True, "message":
+                                            {"context": response.message.content, "sender": "AI", "time": sending_time_format, "fromMe": False }}))
+            sending_message = Message(room=self.room_instance, at_received=received_time, sender=self.user, context=response.message.content, reply_byAI=True)
+            sending_message.save()
+            self.send(text_data=json.dumps({"isMessage": False, "status": "wait4you"}))
+        else:
+            self.send(text_data=json.dumps({"isMessage": False, "status": "wait4you"}))
 
     def chat_message(self, event):
         message = event["message"]
